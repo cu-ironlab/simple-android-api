28d837f5c9d1e269b3d59df2c4a6c1a29ccb3454
BRANDON BARRETT <mrbrandonbarrett@gmail.com>
2016-03-13 23:58:43 -0600

    init, simple image model

---
+++ app/models/image.rb
@@ -0,0 +1,4 @@
+class Image < ActiveRecord::Base
+  has_attached_file :attachment
+  validates_attachment :attachment, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+end
